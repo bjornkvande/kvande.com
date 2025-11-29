c985604df294426887a73f5e9312b3289d15711c
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2025-11-29 13:59:43 +0100

    added more paintings

--- routes/blog/painting.tsx
+++ routes/blog/painting.tsx
@@ -13,24 +13,22 @@ export default function Blog() {
         </P>
 
         <P>
-          The answer provoked and frustrated me, so I thought I would give it a go.
-        </P>
-
-        <P>
-          Around the same time, posts from the Portuguese artist Francisco Fonseca started
-          appearing in my social feed, so I got inspired to try and paint houses. They are
-          just a bunch of geometric shapes and lines with a lot of detail anyway - so how
-          hard could it be.
+          The answer provoked and frustrated me.
         </P>
       </S>
 
       <S>
         <H4>23. august 2025 - first attempt</H4>
         <p>
-          I had just bought a water color set and a few pens the day before, and thought I
-          should give it a go. I had a picture I took from Mondovi, Piemonte earlier in
-          the summer with lots of buildings on it. I got too impatient and wanted to see
-          how it looked with colors, so I never got to finish it.
+          After thinking about this painting thing for a few weeks, and seeing my
+          girlfriend draw a little, I thought I would give it a go. Around the same time,
+          posts from the Portuguese artist{' '}
+          <a href='https://www.instagram.com/_francis.co/'>Francisco Fonseca</a>{' '}
+          started appearing in my social feed, and I got inspired by his paintings of old
+          buildings. I bought a water color set and a few pens and I had a picture I took
+          from Mondovi in Piemonte earlier in the summer with lots of buildings on it.
+          During the process, I got too impatient and wanted to see how it looked with
+          colors, so I never got to finish it. But I liked the style.
         </p>
         <I src='/paintings/first_attempt.jpg' />
       </S>
@@ -101,6 +99,130 @@ export default function Blog() {
           the view from my bar.
         </p>
         <I src='/paintings/view_from_bar.jpg' />
+      </S>
+
+      <S>
+        <H4>6. september 2025 - perspectives gotten right</H4>
+        <p>
+          After messing up the perspectives in the drawing the day before, I thought I
+          would try and do it properly. I sat in place and made a pencil sketch. Took a
+          picture, and went home to finish it. Very happy with the perspective and the
+          shadows in the windows, but the other shadows I messed up.
+        </p>
+        <I src='/paintings/perinaldo_entrance.jpg' />
+        <div className='flex max-w-2xl justify-around'>
+          <image
+            className='w-3/12 rounded shadow'
+            src='/paintings/perinaldo_entrance_1.jpg'
+          />
+          <image
+            className='w-3/12 rounded shadow'
+            src='/paintings/perinaldo_entrance_2.jpg'
+          />
+          <image
+            className='w-3/12 rounded shadow'
+            src='/paintings/perinaldo_entrance_3.jpg'
+          />
+        </div>
+      </S>
+
+      <S>
+        <H4>10. september 2025 - a more cartoony style with perspectives off</H4>
+        <p>
+          I wanted to test out painting a building in a little more cartoonish style with
+          the perspectives deliberately a little bit off. I was very happy with this, but
+          the coloring is not too good - you can clearly see the brush strokes. I would
+          like it to be a little more random.
+        </p>
+        <I src='/paintings/cartoon_style.jpg' />
+      </S>
+
+      <S>
+        <H4>17. september 2025 - messing up light and shadow</H4>
+        <p>
+          This scene had such great light, shadow and colors, but I totally missed the
+          mark on this one. I also need to learn how to paint plants. But the coloring of
+          the walls was not too bad.
+        </p>
+        <I src='/paintings/messing_up_light.jpg' />
+      </S>
+
+      <S>
+        <H4>22. september 2025 - roof tiles, people, shadows and reflections</H4>
+        <p>
+          Allright! I was super happy with this one. I got the shadows more or less
+          correct, painted roof tiles for an hour (crap, a lot), got the human figures
+          placed into the correct perspective with ok balance and proportions. The ground
+          was wet, and I tried to paint reflections onto the ground - not too bad. I also
+          used a white marker for highlights and reflections. The bike was off - have to
+          get back to that.
+        </p>
+        <I src='/paintings/kranj.jpg' />
+      </S>
+
+      <S>
+        <H4>28. september 2025 - more perspectives and structure on walls</H4>
+        <p>
+          Wanted to see if I could make the walls of buildings look more organic with some
+          structure. Quite happy with the walls, the perspective of the staircase, and the
+          shadows on the shutters and the green door. But again put too much ink on the
+          page. I need to get a thinner pen, a brown one.
+        </p>
+        <I src='/paintings/wall_structure.jpg' />
+      </S>
+
+      <S>
+        <H4>7. october 2025 - bigger format and more details</H4>
+        <p>
+          Moving away from my sketchbook I wanted to check out bigger formats and more
+          details. This is an unfinished sketch of the old town of Menton, France. Quite
+          happy with the level of details and the perspective.
+        </p>
+        <I src='/paintings/menton.jpg' />
+      </S>
+
+      <S>
+        <H4>8. october 2025 - coloring was a mistake</H4>
+        <p>
+          I added a the missing piece and a couple of trees in the top right corner and
+          was very happy with the trees (I can do trees now). However, painting the scene
+          was a total mistake. It looked a lot better as a black and white pen drawing.
+        </p>
+        <I src='/paintings/menton_colors.jpg' />
+      </S>
+
+      <S>
+        <H4>9. october 2025 - painting a leaf in 30 minutes</H4>
+        <p>
+          After spending several days with all those details from Menton, and not being
+          happy after coloring it, I picked up a leaf from the street and painted it.
+          Added a shadow for more depth. It took about 30 minutes. Again I thought "did I
+          make this???"
+        </p>
+        <I src='/paintings/leaf.jpg' />
+      </S>
+
+      <S>
+        <H4>12. october 2025 - messing up light and shadow again!</H4>
+        <p>
+          Inspired by the leaf painting a few days earlier and the amazing autumn colors,
+          I wanted to have another go at trying to capture light and shadow. And I totally
+          messed it up again. Yuk! But I liked the tree.
+        </p>
+        <I src='/paintings/sanromolo.jpg' />
+      </S>
+
+      <S>
+        <H4>23. october 2025 - and there was finally light and shadow</H4>
+        <p>
+          A few days earlier I took my mountain bike to the woods between San Romolo and
+          Bajardo. The forest road was just filled with leafs and the light was amazing. I
+          took a photo of my bike and thought I could have another go at the light. This
+          time it worked. Super happy! The bike also looked ok. And oh, do you see the
+          small shadow underneath the wheels? Without those tiny shadows, the bike was
+          floating in the air. You can`t fool the brain.
+        </p>
+        <I src='/paintings/bike.jpg' />
       </S>
     </BlogPage>
   );
