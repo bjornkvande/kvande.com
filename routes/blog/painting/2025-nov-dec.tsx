d4e30cf13507913eb00039c783213f5727e7d4f9
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2025-12-13 14:48:38 +0100

    updated the text

--- routes/blog/painting/2025-nov-dec.tsx
+++ routes/blog/painting/2025-nov-dec.tsx
@@ -25,10 +25,11 @@ export default function Blog() {
         <H3>1. november 2025 - quick test</H3>
         <p>
           Sitting on the ferry from Denmark to Norway with my sketch book and my alcohol
-          markers I made a quick attemt on a portrait. I first sketched with a pencil,
-          then added the shadows gradually from light to dark. Then erased the pencil in
-          the end. The resulting portrait did not resemble the person at all, but at least
-          it looked like a person.
+          markers I made a quick attempt at a portrait. I made a quick sketch with a
+          pencil, then added the shadows gradually from light to dark. Then erased the
+          pencil in the end - to quickly learn that it was impossible where I had actually
+          used the alcohol markers. The resulting portrait did not resemble the person at
+          all, but at least it looked like a person with somewhat correct proportions.
         </p>
         <I src='/paintings/stian_fast.jpg' />
       </S>
@@ -36,12 +37,14 @@ export default function Blog() {
       <S>
         <H3>7. november 2025 - dirty mechanic</H3>
         <p>
-          I had a photo of my friend Sjur from back in the days after he had been working
-          on my old car that I wanted to try and paint using my alchohol markers. Very
-          satisfied with the t-shirt and fingers. Totally missed the portrait likeness for
-          the face. The head is a little too small and the eyes too large, and something
-          wrong with the nose. The super-man symbol turned out very nice. The oil smudges
-          were added with water color at the end.
+          I had a black and white photo of a friend from back in the days after he had
+          been working on my old car. I really like the photo and I wanted to try and draw
+          it using my alchohol markers. I am very satisfied with the t-shirt and the
+          fingers. But I totally missed the portrait likeness for the face. The head is a
+          little too small and the eyes too large. And there is something wrong with the
+          nose. But the balance of the figure looks realistic. The superman symbol turned
+          out very nice. The oil smudges on the arm and fingers were added with water
+          color at the end. Two dots with a white pen for the eyes.
         </p>
         <I src='/paintings/sjur_mechanic.jpg' />
       </S>
@@ -49,10 +52,11 @@ export default function Blog() {
       <S>
         <H3>11. november 2025 - portrait likeness</H3>
         <p>
-          Not giving up at portrait likeness I had another go. This time with pen and
-          watercolor, and in color. Very happy with the shadows and the intensity of the
-          eyes. The shirt also turned out ok, but the beard - not so much. But I like the
-          picture.
+          Not giving up at portrait likeness I had another go at the same person. This
+          time in color with pen and watercolor. I am very happy with the shadows of the
+          face and the intensity of the eyes. I understood the values of the colors are
+          more important than the actual colors. The shirt also turned out ok. The beard -
+          not so much. But I like the picture.
         </p>
         <I src='/paintings/sjur.jpg' />
       </S>
@@ -60,12 +64,12 @@ export default function Blog() {
       <S>
         <H3>14. november 2025 - doom, gloom and some hope</H3>
         <p>
-          This is a scene from the Opera in Oslo. My plan was to try and make a dark and
-          gloomy image in black and white with a small figure of hope looking into the
-          sky. The person has the right proportions and balance. Some even said it looked
-          like my girlfriend. Made an attempt to add the something that could look like a
-          scull in the top right corner. Quite happy with the result of gloom and doom in
-          the painting. Pen and water colors.
+          This is a scene from the roof of the Opera in Oslo. My plan was to try and make
+          a dark and gloomy image in black and white with a small colored figure of hope
+          looking into the sky. The person has the right proportions and balance. Some
+          even said it looked like my girlfriend (which it is). I made an attempt at
+          adding something that would resemble a scull in the top right corner. Quite
+          happy with the result of gloom and doom in the painting. Pen and water colors.
         </p>
         <I src='/paintings/opera.jpg' />
       </S>
@@ -73,13 +77,13 @@ export default function Blog() {
       <S>
         <H3>21. november 2025 - brush pen</H3>
         <p>
-          This picture started its life as a charcoal drawing, and the plan was to use a
-          brush pen to make thin and thick strokes in one direction only. It turned out it
-          was so difficult to control the brush tip that I more or less gave up that
-          project. So it ended up with short brush strokes and an attempt at shadowing.
-          The head is supposed to totally be in the shadow, but it just looks strange. And
-          the breast is out of proportions, and it looks like one is missing. I like the
-          nipple :)
+          This picture started as a charcoal drawing, and the plan was to use a brush pen
+          to make thin and thick strokes in one direction only to make light and shadow.
+          Then erase the charcoal in the end. It turned out it was so difficult to control
+          the brush tip that I more or less gave up that project. It ended up with short
+          and messy brush strokes instead. The head is supposed to be totally in the
+          shadow, but it just ended up looking strange. The breast is out of proportions,
+          and it looks like one is missing. But I like the nipple :)
         </p>
         <I src='/paintings/summer.jpg' />
       </S>
@@ -90,8 +94,8 @@ export default function Blog() {
           Time for a selfie :) I am super happy with this one. The likeness is good -
           well, at least how I looked back in 2012. It is based on a selfie I made after a
           very cold and dirty bike ride, hence the expression. Very happy with the nose,
-          eyes, beard, and hair. Tried to make some smudges where the shadows are supposed
-          to be. Brown pencil.
+          eyes, beard, and hair. Tried to make some smudges where the shadows go. Brown
+          pencil.
         </p>
         <I src='/paintings/selfie.jpg' />
       </S>
@@ -99,13 +103,13 @@ export default function Blog() {
       <S>
         <H3>27. november 2025 - first attempt made proper</H3>
         <p>
-          I wanted to try a proper attempt at the portrait I tried doing back on the
-          ferry. Very happy with this. The likeness is very good, and I just love how the
-          glasses turned out. I was very afraid I would mess up the hair (with alcohol
-          markers you can not fix mistakes very easily) and have it not look blond, but it
-          ended up good. The shadows under the nose and chin also worked out nicely. Made
-          a couple of mistakes but you have to look for them yourself. Pencil and alcohol
-          markers with a dash of white at the end (glasses).
+          I wanted to try a proper attempt at the quit portrait I made four weeks earlier.
+          Very happy with this. The likeness is very good, and I just love how the glasses
+          turned out. I was afraid I would mess up the hair (with alcohol markers you can
+          not fix mistakes very easily) and have it not look blond, but it ended up nice
+          and fluffy. The shadows under the nose and chin also worked out. I dade a couple
+          of mistakes but you have to look for them yourself. Pencil and alcohol markers
+          with a dash of white marker at the end (glasses and shirt button).
         </p>
         <I src='/paintings/stian.jpg' />
       </S>
@@ -116,10 +120,10 @@ export default function Blog() {
           I had a picture of a friend playing a fretless bass. The positure was quite
           complex, but I wanted to have a go. I sketched it out with a pencil to get the
           proportions and balance correct, followed loosely with a pen for a more organic
-          and messy look. Erased the pencil, and then colored it with water colors. I used
-          two pens, one of them was not water proof, so I the pen started bleeding around
-          the face and he turned a little green. Note to self: check if the pen is water
-          proof first. Messed up the glasses, but there is absolutely some portrait
+          and messy look. I erased the pencil, and then colored it with water colors. I
+          used two pens, one of them was not water proof, so the pen started bleeding
+          around the face and he turned green. Note to self: check if the pen is water
+          proof first. I messed up the glasses, but there is absolutely some portrait
           likeness there. Very happy with the shadows of the crinkles and folds of the
           pants and shirt. Fingers also turned out nice, except that he got green fingers.
           I like the shoes. The bass might look small, but it was a hand-built small bass.
